95ae10fbe599329c2672f42cf6c331fc55127411
Kestutis Burba <kburba@gmail.com>
2018-10-09 23:17:13 +0300

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-module.exports = {
-  // mongoURI: 'mongodb://devcon:devcon12345@ds125502.mlab.com:25502/devconnectordb12345',
-  mongoURI: "mongodb://localhost:27017/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+}
+{
+  module.exports = require("./keys_dev");
+}
