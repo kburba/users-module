8f622ffb39ea609c60f4b3aeeaa9b58a2be8711e
KESTUTIS BURBA <kestutis@ynot-media.com>
2021-01-03 14:25:45 +0200

    users controller

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,57 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
 const passport = require("passport");
 
-// load input validation
-const validateRegisterInput = require("../../validation/register");
-
 // load user model
-const User = require("../../modules/User");
+const UsersController = require("../../controllers/UsersController.ts");
 
 // @ROUTE POST api/users/register
 // @DESC Register user
 // @ACCESS Public
 
-router.post("/register", (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  User.findOne({
-    email: req.body.email,
-  })
-    .then((user) => {
-      if (user) {
-        errors.email = "email already exists";
-        res.status(400).json(errors);
-      } else {
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        });
-
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) {
-              return err;
-            }
-            newUser.password = hash;
-            newUser
-              .save()
-              .then((user) => {
-                res.json(user);
-              })
-              .catch((err) => console.log(err));
-          });
-        });
-      }
-    })
-    .catch((err) => console.log(err));
-});
+router.post("/register", UsersController.register);
 
 // @ROUTE   POST /api/users/me
 // @DESC    Return logged user info
@@ -62,9 +20,7 @@ router.get(
   passport.authenticate("jwt", {
     session: false,
   }),
-  (req, res) => {
-    res.json(req.user);
-  }
+  UsersController.me
 );
 
 module.exports = router;
